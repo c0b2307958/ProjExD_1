a8b1087e1e07368bfc5074427b30fd67f6f9144c
fushimity <fushimity@edu.teu.ac.jp>
2024-11-19 17:02:37 +0900

    練習：８

--- pygame_image.py
+++ pygame_image.py
@@ -4,43 +4,46 @@ import pygame as pg
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
-    clock  = pg.time.Clock()
+    clock = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     bg_img2 = pg.transform.flip(bg_img, True, False)
     tmr = 0
+    kk_rct = kk_img.get_rect()  
+    kk_rct.center = (300, 200)  
+
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: return
+            if event.type == pg.QUIT:
+                return
 
         x = tmr % 4800
         key_lst = pg.key.get_pressed()
         if key_lst[pg.K_UP]:
-            kk_img.move_ip((0,1))
-        if key_lst[pg.K_DOWN]:
-            kk_img.move_ip((0,-1))
-        if key_lst[pg.K_LEFT]:
-            kk_img.move_ip((1,0))
+            kk_rct.move_ip(0, -1)
+        if key_lst[pg.K_DOWN]: 
+            kk_rct.move_ip(0, 1)
+        if key_lst[pg.K_LEFT]:  
+            kk_rct.move_ip(-1, 0)
         if key_lst[pg.K_RIGHT]:
-            kk_img.move_ip((-1,0))
-        screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img2, [-x+1600, 0])
-        screen.blit(bg_img, [-x+3200, 0])
-        screen.blit(bg_img2, [-x+4800, 0])
-        screen.blit(kk_img, [300, 200])
+            kk_rct.move_ip(1, 0)
+
         
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img2, [-x + 1600, 0])
+        screen.blit(bg_img, [-x + 3200, 0])
+        screen.blit(bg_img2, [-x + 4800, 0])
+
+        
+        screen.blit(kk_img, kk_rct) 
+
         pg.display.update()
         tmr += 1
-        # if tmr == 800:
-        #     tmr -= 800
-        y = 2000
-        clock.tick(y)
-
+        clock.tick(200)  
 
 if __name__ == "__main__":
     pg.init()
